25da193f872fc689b9f88bb349e29486a1737b99
JakeLawhorne <jake.lawhorne@gmail.com>
2024-02-28 02:23:15 -0500

    chars and ints finished

--- src/main/java/com/github/curriculeon/StringParser.java
+++ src/main/java/com/github/curriculeon/StringParser.java
@@ -1,5 +1,6 @@
 package com.github.curriculeon;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -8,11 +9,26 @@ import java.util.List;
 public class StringParser {
 
     public static Character[] parseCharacters(String input) {
-        return null;
+        if(input == null) return null;
+        Character[] characters = new Character[input.length()];
+        char[] chars = input.toCharArray();
+        for(int i = 0; i < input.length(); i++){
+            characters[i] = chars[i];
+        }
+        return characters;
     }
 
     public static Integer[] parseIntegers(String input) {
-        return null;
+        if(input == null) return null;
+
+        ArrayList<Integer> integers = new ArrayList<>();
+        String[] strings = input.split("");
+        for(int i = 0; i < input.length(); i++){
+            if(strings[i].equals(" ")) continue;
+            integers.add(Integer.parseInt(strings[i]));
+        }
+        Integer[] result = integers.toArray(new Integer[integers.size()]);
+        return result;
     }
 
     public static List<String> toList(String[] input) {
